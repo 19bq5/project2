b831b9bedb167c27af171139500706117035a58b
bindhureddy <himabindhu9490gmail.com>
2020-03-12 15:40:24 +0530

    Updates

---
+++ precache-manifest.19bb8682e6de61cddd01c79a76fa8538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3827df97dce85e2050fca7e1cb4a5e3",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "cf175eab95990f76a318",
+    "url": "/project2/static/css/main.8f869dc3.chunk.css"
+  },
+  {
+    "revision": "5ad237f86a02ef863cc1",
+    "url": "/project2/static/js/2.ca39ed0f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project2/static/js/2.ca39ed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf175eab95990f76a318",
+    "url": "/project2/static/js/main.0c104b6b.chunk.js"
+  },
+  {
+    "revision": "5eaec05a958effd1ddf7",
+    "url": "/project2/static/js/runtime-main.3df5eaf6.js"
+  },
+  {
+    "revision": "efe01b90316cc1eca182c240aa38b1f8",
+    "url": "/project2/static/media/boy.efe01b90.svg"
+  }
+]);
